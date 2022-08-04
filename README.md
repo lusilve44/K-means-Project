073d30218203c745d53e36c0896cbf0896b0ec40
Luisina Silvestre <luisina.silvestre0914@gmail.com>
2022-08-04 02:33:32 +0000

    terminado

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Template for Machine Learning projects
+# K-means Project
+
+As an excersice of implementation of th K-means algorithm, we try to group in 6 different clusters the houses that are registred in the following dataset (https://raw.githubusercontent.com/4GeeksAcademy/k-means-project-tutorial/main/housing.csv), based on coordinates and median income.
--- src/app.py
+++ src/app.py
@@ -1,4 +1,22 @@
-from utils import db_connect
-engine = db_connect()
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+from sklearn.preprocessing import StandardScaler
 
-# your code here
+df = pd.read_csv('/workspace/K-means-Project/data/df_data.csv')
+
+scaler = StandardScaler()
+df_scaled = scaler.fit_transform(df)
+
+km_6 = KMeans(n_clusters=6, random_state=42)
+km_6.fit(df_scaled)
+
+df_inv = scaler.inverse_transform(df_scaled)
+df_inv = pd.DataFrame(df_inv,columns=['ind','Latitude','Longitude','MedInc'])
+df_inv['Cluster'] = km_6.labels_
+
+df_inv['Cluster'] = pd.Categorical(df_inv.Cluster)
+
+model_inertia = km_6.inertia_
